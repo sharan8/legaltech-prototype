56f751fa2c659cbd619e9b4d0649dcae85608560
ongyz <ongyanzhi96@gmail.com>
2018-09-11 00:56:58 +0800

    Fix button to load coa.html
    Commented out creation of problems model in views.index()

--- legaltech_prototype/english/views.py
+++ legaltech_prototype/english/views.py
@@ -4,16 +4,17 @@ from .models import Problem
 
 
 def index(request):
-    neighbor_dispute = Problem()
-    neighbor_dispute.name = "Neighbor Dispute"
-    neighbor_dispute.coa = "You need to fill up a form in English"
-    neighbor_dispute.save()
+##    neighbor_dispute = Problem()
+##    neighbor_dispute.name = "Neighbor Dispute"
+##    neighbor_dispute.coa = "You need to fill up a form in English"
+##    neighbor_dispute.save()
     return render(request, 'english/index.html')
 
 
 def coa_view(request):
-    coa_object = Problem.objects.filter(name="Neighbor Dispute")
+    coa_object = Problem.objects.get(name="Neighbor Dispute")
     context = {
         'coa_object': coa_object
     }
+    print(context)
     return render(request, 'english/coa.html', context)
